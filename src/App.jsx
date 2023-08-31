065506c8a70ae51dd4a0e38e91552c3dfd8cc47d
Instrutor Maceio <instrutor_mcz@SECTI.LOCAL>
2023-08-31 13:21:42 -0300

    commit aula5

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,29 @@ import './App.css'
 import Banner from './components/Banner';
 
 import Formulario from './components/formulario';
+import Time from './components/time';
 
 function App() {
 
   const [colaboradores, setColaboradores] = useState([])
 
   const AddColaborador = (colaborador) => {
-    setColaboradores([...colaboradores, colaborador]) 
+    setColaboradores([...colaboradores, colaborador])
   }
 
 
   return (
-    
+
     <div >
       <Banner />
       <div className='container'>
-      <Formulario novoColaborador={colaborador => AddColaborador(colaborador)} />
+        <Formulario novoColaborador={colaborador => AddColaborador(colaborador)} />
+        <Time nome="Programação" />
+        <Time nome="FrontEnd" />
+        <Time nome="Data Science" />
       </div>
-      
-      
+
+
     </div>
   )
 }
---
+++ src/components/time/index.jsx
@@ -0,0 +1,12 @@
+
+import './time.css';
+
+function Time(props){
+    return(
+        <section className="time">
+            <h2>{props.nome}</h2>
+        </section>
+    )
+}
+
+export default Time;
