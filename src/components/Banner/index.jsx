6c7fe0d5faef3f9245ca514b49e94ad8eeb51fa4
Instrutor Maceio <instrutor_mcz@SECTI.LOCAL>
2023-10-03 14:17:55 -0300

    commit final - 2

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import './banner.css'
+import bannerImg from '../../assets/HeaderHeader_total.png'
 
 function Banner(){
     return(
         <header className="header_banner">
-            <img src="./src/assets/HeaderHeader_total.png"/>
+            <img src={bannerImg}/>
         </header>
     )
 }
